b3e3bf1b0bc3dbcf7dbdf487ca2e4e0e87bb7e3d
criseda <laurentiu.cristian.preda@gmail.com>
2025-06-01 00:09:56 +0100

    Add StartFuzzing method to Core class for executing fuzzing loops

--- inc/VulneraTestX.hpp
+++ inc/VulneraTestX.hpp
@@ -5,6 +5,13 @@ namespace VulneraTestX {
     public:
         void Initialize();
 
+        /**
+         * @brief Starts a basic fuzzing loop against the specified target executable.
+         * @param targetExecutablePath Path to the executable to fuzz.
+         * @param numIterations Number of fuzzing iterations to perform.
+         */
+        void StartFuzzing(const std::string& targetExecutablePath, int numIterations);
+
     protected:
     private:
     };
